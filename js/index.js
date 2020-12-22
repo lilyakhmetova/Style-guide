151ffbdd30bbad98c974045c21c6a1380f0ae05d
Lily Akhmetova <55274620+lilyakhmetova@users.noreply.github.com>
2020-12-22 23:46:10 +0300

    мобильная версия

---
+++ js/index.js
@@ -0,0 +1,47 @@
+document.querySelector('.burger').onclick = function() {
+  document.querySelector('.burger').style.display = `none`
+  document.querySelector('.cross').style.display = `block`
+  document.querySelector('.fullmenu').style.display = `flex`
+}
+
+document.querySelector('.cross').onclick = function() {
+  document.querySelector('.burger').style.display = `inherit`
+  document.querySelector('.cross').style.display = `none`
+  document.querySelector('.fullmenu').style.display = `none`
+}
+
+document.querySelector(".hide").onclick = function() {
+  document.querySelector('.fullmenu').style.display = `none`
+  document.querySelector('.burger').style.display = `inherit`
+  document.querySelector('.cross').style.display = `none`
+}
+document.querySelector(".hide2").onclick = function() {
+  document.querySelector('.burger').style.display = `inherit`
+  document.querySelector('.cross').style.display = `none`
+  document.querySelector('.fullmenu').style.display = `none`
+}
+document.querySelector(".hide3").onclick = function() {
+  document.querySelector('.burger').style.display = `inherit`
+  document.querySelector('.cross').style.display = `none`
+  document.querySelector('.fullmenu').style.display = `none`
+}
+document.querySelector(".hide4").onclick = function() {
+  document.querySelector('.burger').style.display = `inherit`
+  document.querySelector('.cross').style.display = `none`
+  document.querySelector('.fullmenu').style.display = `none`
+}
+document.querySelector(".hide5").onclick = function() {
+  document.querySelector('.burger').style.display = `inherit`
+  document.querySelector('.cross').style.display = `none`
+  document.querySelector('.fullmenu').style.display = `none`
+}
+document.querySelector(".hide6").onclick = function() {
+  document.querySelector('.burger').style.display = `inherit`
+  document.querySelector('.cross').style.display = `none`
+  document.querySelector('.fullmenu').style.display = `none`
+}
+document.querySelector(".hide7").onclick = function() {
+  document.querySelector('.burger').style.display = `inherit`
+  document.querySelector('.cross').style.display = `none`
+  document.querySelector('.fullmenu').style.display = `none`
+}
